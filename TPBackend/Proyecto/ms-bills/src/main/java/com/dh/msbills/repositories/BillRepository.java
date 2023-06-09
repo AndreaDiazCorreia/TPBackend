71794ab7df63ba3538a349a952235f70f5af2fd9
Andrea Díaz Correia <85651492+AndreaDiazCorreia@users.noreply.github.com>
2023-06-09 17:31:26 -0300

    Delete TPBackend/Proyecto directory

--- TPBackend/Proyecto/ms-bills/src/main/java/com/dh/msbills/repositories/BillRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dh.msbills.repositories;
-
-import com.dh.msbills.models.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BillRepository extends JpaRepository<Bill, String> {
-}
